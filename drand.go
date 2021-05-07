95d044439d261f395015295422eefd7d2c0483fc
刘勇 <liuyong@tianrukj.com>
2021-05-07 13:40:42 +0800

    reset config

--- drand.go
+++ drand.go
@@ -4,13 +4,14 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-// const GenesisTimeStamp = 1598306400
-const GenesisTimeStamp = 1613776200
-
-// var DSchedule = map[abi.ChainEpoch]DrandEnum{
-// 	51000: DrandMainnet,
-// }
+const GenesisTimeStamp = 1598306400
 
 var DSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,
+	51000: DrandMainnet,
 }
+
+// const GenesisTimeStamp = 1613776200
+
+// var DSchedule = map[abi.ChainEpoch]DrandEnum{
+// 	0: DrandMainnet,
+// }
