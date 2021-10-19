bf46fcb7da726ff227f02f8496be7b67b06fa30a
lyswifter <ly70835@163.com>
2021-10-19 16:36:19 +0800

    switch to calibrabnet

--- drand.go
+++ drand.go
@@ -3,15 +3,15 @@ package main
 import "github.com/filecoin-project/go-state-types/abi"
 
 // main network
-const GenesisTimeStamp = 1598306400
-
-var DSchedule = map[abi.ChainEpoch]DrandEnum{
-	51000: DrandMainnet,
-}
-
-// calibration network
-// const GenesisTimeStamp = 1624060800
+// const GenesisTimeStamp = 1598306400
 
 // var DSchedule = map[abi.ChainEpoch]DrandEnum{
-// 	0: DrandMainnet,
+// 	51000: DrandMainnet,
 // }
+
+// calibration network
+const GenesisTimeStamp = 1624060800
+
+var DSchedule = map[abi.ChainEpoch]DrandEnum{
+	0: DrandMainnet,
+}
